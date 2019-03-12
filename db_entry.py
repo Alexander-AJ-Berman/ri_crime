db3455227909b0c84b0acfbcb1b00396414873c9
Alexander-AJ-Berman <alexander_berman@brown.edu>
2019-03-12 15:12:30 -0400

    data entry

---
+++ db_entry.py
@@ -0,0 +1,37 @@
+import csv
+import json
+import pandas as pd 
+import sys, getopt, pprint
+from pymongo import MongoClient
+
+uri = 'mongodb://user:password1@ds159025.mlab.com:59025/ri_crime_data'
+
+def main():
+    client = MongoClient(uri)
+
+    db = client.get_default_database()
+    db_cm = db['cases']
+
+    data = pd.read_csv("./PPCL_180days.csv")
+    data_json = json.loads(data.to_json(orient='records'))
+    db_cm.insert(data_json)
+
+    # header = [
+    #     "CaseNumber",
+    #     "Location",
+    #     "Reported Date",
+    #     "Month",
+    #     "Year",
+    #     "Offense Desc",
+    #     "Statute Code",
+    #     "Statute Desc",
+    #     "Counts",
+    #     "Reporting Officer"
+    # ]
+
+
+
+
+
+if __name__ == "__main__":
+    main()
