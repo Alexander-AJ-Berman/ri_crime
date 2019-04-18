1ede091be8439bb75e41b02b962625c832ece95b
Kevin Du <kevin_du@brown.edu>
2019-04-18 16:01:49 -0400

    added stubs for simple viz's

---
+++ visualizations/time_breakdown.py
@@ -0,0 +1,39 @@
+def plot_month(db_cases, db_arrests):
+    """
+    x-axis: each month
+    y-axis: num_cases, num_arrests (2 separate lines, double y-axis)
+    :param db_cases: cases
+    :param db_arrests: arrests
+    :return: line plot or histogram
+    """
+    pass
+
+def plot_day_of_week(db_cases, db_arrests):
+    """
+    x-axis: each day of week
+    y-axis: num_cases, num_arrests (2 separate lines, double y-axis)
+    :param db_cases: cases
+    :param db_arrests: arrests
+    :return: histogram
+    """
+    pass
+
+def plot_time_of_day(db_cases, db_arrests):
+    """
+    x-axis: each hour
+    y-axis: num_cases, num_arrests (2 separate lines, double y-axis)
+    :param db_cases: cases
+    :param db_arrests: arrests
+    :return: histogram
+    """
+    pass
+
+def plot_every_day(db_cases, db_arrests):
+    """
+    x-axis: each day from december to april
+    y-axis: num_cases, num_arrests (2 separate lines, double y-axis)
+    :param db_cases: cases
+    :param db_arrests: arrests
+    :return: line plot or histogram
+    """
+    pass
