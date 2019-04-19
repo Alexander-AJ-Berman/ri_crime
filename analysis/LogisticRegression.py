d912cbfe900c8ee0c6e32e1552d8656a64ca6a52
Kevin Du <kevin_du@brown.edu>
2019-04-19 12:46:09 -0400

    added mongo -> pandas df line

--- analysis/LogisticRegression.py
+++ analysis/LogisticRegression.py
@@ -7,10 +7,22 @@ from pymongo import MongoClient
 uri = 'mongodb://user:password1@ds159025.mlab.com:59025/ri_crime_data'
 
 client = MongoClient(uri)
-
 db = client.get_database()
 db_cases = db['cases']
-db_arrests = db['arrests']
+
+def mongo_to_df():
+    """
+    Converts db_cases to a pandas df
+    :return: pandas df
+    """
+    df = pd.DataFrame(list(db_cases.find()))
+    return df
+
+    """
+    # helpful other commands to look at data
+    print(db_cases.find_one()) # prints one case from the mongo db
+    print(df.iloc[0]) # prints the 0th row from the pandas df
+    """
 
 def return_statute_code_features():
     statute_codes = set()
@@ -26,6 +38,7 @@ def return_statute_code_features():
 
 """
 TODOs:
+0. Convert mongo db_cases into a pandas dataframe - done [Kevin]
 1. Get the right features (month, hour, weekday vs weekend/weekdays, statute code, reporting officer, counts)
     a. convert categories into columns
         i. date -> day of the week -> [0, ..., 1]/[0, 1] (if weekend/weekday)
@@ -43,8 +56,15 @@ TODOs:
     a. class_weight = balanced
 4. Get new data, put in db as db_test_cases, db_test_arrests
     - Kevin
+        - instead, use train_test_split method from sklearn
 5. LogisticRegression.train(test_data)
 6. Analyze results:
     a. Make precision-recall graph
 
 """
+
+def main():
+    df = mongo_to_df()
+
+if __name__ == "__main__":
+    main()
