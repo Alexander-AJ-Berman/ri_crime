409439cbb308211d3b3d440de3119634f2724387
Jeffrey Zhong <jeffreyzhong@Jeffreys-MacBook-Pro.local>
2019-04-19 18:19:59 -0400

    cleaned all data

--- analysis/LogisticRegression.py
+++ analysis/LogisticRegression.py
@@ -2,7 +2,9 @@ import csv
 import json
 import pandas as pd 
 import sys, getopt, pprint
+import pymongo
 from pymongo import MongoClient
+import datetime
 
 uri = 'mongodb://user:password1@ds159025.mlab.com:59025/ri_crime_data'
 
@@ -24,17 +26,90 @@ def mongo_to_df():
     print(df.iloc[0]) # prints the 0th row from the pandas df
     """
 
-def return_statute_code_features():
+def convert_data(df):
+    data = []
+    columns = df.columns;
+    for index, row in df.iterrows():
+        tmp = {}
+        for col in columns:
+            if col != 'lat' and col != 'lon':
+                tmp[col] = row[col]
+        data.append(tmp)
+    return data 
+
+def clean_data(data):
+    ml_data = []
+    labels = []
+    officers = get_officers(data)
+    statute_codes = get_statute_codes()
+    for row in data:
+        tmp = []
+        tmp.append(row['Month'])
+        tmp.append(convert_to_hour(row['Reported Date']))
+        tmp.append(convert_to_day_of_the_week(row['Reported Date']))
+        tmp.append(convert_statute_code(row['Statute Code'],statute_codes))
+        tmp.append(convert_reporting_officer(row['Reporting Officer'],officers))
+        tmp.append(row['Counts'])
+        tmp.append(row['latitude'])
+        tmp.append(row['longitude'])
+        ml_data.append(tmp)
+        if row['Arrests'] != []:
+            labels.append(1)
+            print(row['Arrests'])
+        else:
+            labels.append(0)
+
+    return ml_data, labels
+
+def get_officers(data):
+    officers = set()
+    for row in data:
+        officers.add(row['Reporting Officer'])
+    officer_dict = {}
+    num = 0
+    for officer in list(officers):
+        officer_dict[officer] = num
+        num += 1
+    return officer_dict
+
+def convert_to_hour(date):
+    split_date = date.split()
+    hour = int(split_date[1][:2])
+    if split_date[2] == 'PM':
+        hour += 12
+    return hour
+
+def convert_to_day_of_the_week(date):
+    one_hot_vector = [0] * 7
+    split_date = date.split()[0]
+    split_date = split_date.split('/')
+    day_of_the_week = datetime.datetime(int(split_date[2]),int(split_date[0]),int(split_date[1])).weekday()
+    one_hot_vector[day_of_the_week] = 1
+    return one_hot_vector
+
+def convert_reporting_officer(officer,officers):
+    one_hot_vector = [0] * len(officers.keys())
+    one_hot_vector[officers[officer]] = 1
+    return one_hot_vector
+
+def convert_statute_code(statute,statutes):
+    one_hot_vector = [0] * len(statutes.keys())
+    one_hot_vector[statutes[statute[:4]]] = 1
+    return one_hot_vector
+
+def get_statute_codes():
     statute_codes = set()
     for case in db_cases.find():
         statute_code = case["Statute Code"]
-        split = case["Statute Code"].split("-")
-        # statute_codes.add(statute_code[:5])
-        statute_codes.add(split[0]) 
-
-    print(statute_codes)
-    print(len(statute_codes))
-    return statute_codes
+        # split = case["Statute Code"].split("-")
+        statute_codes.add(statute_code[:4])
+        # statute_codes.add((split[0].strip(),split[1].strip() if len(split) > 1 else '')) 
+    statute_dict = {}
+    num = 0
+    for code in statute_codes:
+        statute_dict[code] = num
+        num += 1
+    return statute_dict
 
 """
 TODOs:
@@ -65,6 +140,8 @@ TODOs:
 
 def main():
     df = mongo_to_df()
+    data = convert_data(df)
+    ml_data, labels = clean_data(data)
 
 if __name__ == "__main__":
     main()
